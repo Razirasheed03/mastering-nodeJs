ab673a2009dfc783cd5d62260d7c41a154405abc
Razirasheed03 <razirasheed03@gmail.com>
2025-05-17 17:56:30 +0530

    changes made

--- stream.js
+++ stream.js
@@ -1,16 +1,17 @@
 const fs=require('fs')
-// const writable=fs.createWriteStream('streamdata.txt')
-// writable.write('data written from stream.js file')
-// writable.end
+const writable=fs.createWriteStream('streamdata.txt')
+writable.write('data written from stream.js file')
+writable.end
 
 
-// const readable=fs.createReadStream('streamdata.txt')
-// readable.on('data',(data)=>{
-//     console.log('datas from streamdata reading : ',data.toString())
-// })
+const readable=fs.createReadStream('streamdata.txt')
+readable.on('data',(data)=>{
+    console.log('datas from streamdata reading : ',data.toString())
+})
 
 const readStream=fs.createReadStream('streamdata.txt')
 const writeStream=fs.createWriteStream('sample.txt')
 
 readStream.pipe(writeStream)
+
 //////copied data from the readfile and pasted in writefile
