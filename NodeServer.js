30e835ac30f450f39b91bcef7a0c667e2c25a58d
Razirasheed03 <razirasheed03@gmail.com>
2025-05-30 17:02:11 +0530

    small changes and restructured pages

--- NodeServer.js
+++ NodeServer.js
@@ -1,12 +1,12 @@
-const http=require('http')
+const http = require('http')
 
-const os=require('os')
-const server=http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/plain'})
+const os = require('os')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'content-type': 'text/plain' })
     res.end('hii')
 })
 
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('server on port 3000')
 })
--- ReadWhenPromiseResolve.js
+++ ReadWhenPromiseResolve.js
@@ -1,7 +1,7 @@
-const fs=require('fs')
+const fs = require('fs')
 
-const m1=new Promise((resolve,reject)=>{
-    resolve(fs.readFile('output.txt',(err,data)=>{
-        console.log('read',data.toString())
+const m1 = new Promise((resolve, reject) => {
+    resolve(fs.readFile('output.txt', (err, data) => {
+        console.log('read', data.toString())
     }))
 })
