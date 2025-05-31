0c7fe0d7ef6869c27f9c80617c558becde4be5b0
Razirasheed03 <razirasheed03@gmail.com>
2025-05-31 17:18:40 +0530

    small changes made

--- stream.js
+++ stream.js
@@ -1,15 +1,15 @@
-const fs=require('fs')
-const writable=fs.createWriteStream('streamdata.txt')
+const fs = require('fs')
+const writable = fs.createWriteStream('streamdata.txt')
 writable.write('data written from stream.js file')
 writable.end
 
 
-const readable=fs.createReadStream('streamdata.txt')
-readable.on('data',(data)=>{
-    console.log('datas from streamdata reading : ',data.toString())
+const readable = fs.createReadStream('streamdata.txt')
+readable.on('data', (data) => {
+    console.log('datas from streamdata reading : ', data.toString())
 })
 
-const readStream=fs.createReadStream('streamdata.txt')
-const writeStream=fs.createWriteStream('sample.txt')
+const readStream = fs.createReadStream('streamdata.txt')
+const writeStream = fs.createWriteStream('sample.txt')
 
 readStream.pipe(writeStream)
